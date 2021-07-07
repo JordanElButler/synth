3a561d8832ffa0c038423e48f2f997c7ba2d04ce
Jordan Butler <blah@blah.org>
2021-07-06 23:38:16 -0700

    fixed a few things

--- synth.js
+++ synth.js
@@ -1,15 +1,18 @@
+  
 var c, ctx, a, gain, osc;
 
 var keys = [
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.',
 ];
 var factor = Math.pow(2, 1/12);
-var noteName = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"];
+var noteNames = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"];
 var rootName = "A";
-var root = 440 * Math.pow(factor, noteName.indexOf(rootName));
-var chromatic = [1,1,1,1,1,1,1,1,1,1,1,1];
+var rootKey = 'f';
+var rootFreq = 440 * Math.pow(factor, noteNames.indexOf(rootName));
+
+var chromatic = [1,1,1,1,1,1,1,1,1,1,1];
 var diatonic = [2,1,2,2,2,1,2];
 var gypsy = [1,3,1,2,1,3,1];
 var sc3 = [1,2,1,4,1,2,1];
@@ -21,19 +24,27 @@ var sc8 = [1,1,3,2,3,1,1];
 var sc9 = [2,2,1,2,1,2,2];
 var sc10 = [1,1,1,6,1,1,1];
 
-var scale = diatonic;
-var rootKey = 'q';
+var scales = {
+  chromatic: chromatic,
+  diatonic: diatonic,
+  gypsy: gypsy,
+  sc3: sc3,
+  sc4: sc4,
+  sc5: sc5,
+  sc6: sc6,
+  sc7: sc7,
+  sc8: sc8,
+  sc9: sc9,
+  sc10: sc10
+};
+
+var scale = scales.chromatic;
 var canvasratio = 0.333;
 
-var pressedKey;
-var scaleKeys;
-var keyMap;
+var keySounds = {};
+var pressedKeys = {};
 
 function init() {
-  a = new AudioContext();
-  gain = a.createGain();
-  gain.connect(a.destination);
-  gain.gain.setValueAtTime(0.2, a.currentTime);
 
   c = document.createElement("canvas");
   ctx = c.getContext("2d");
@@ -43,37 +54,76 @@ function init() {
   c.height = canvasratio*c.width;
 
   scaleKeys = getScaleKeys();
+  setScale();
 
   drawKeyboard();
 
   window.addEventListener("resize", function() {
-    c.width = window.innerWidth*4/5;
-    c.height = c.width * canvasratio
+    setSize();
     drawKeyboard();
   }, false);
 
   window.addEventListener("keydown", function(e) {
-    pressedKey = e.key.toLowerCase();
-    if (validKey(pressedKey)) {
-      console.log(pressedKey);
-      var freq = getFreq(pressedKey);
-      osc = a.createOscillator();
-      osc.frequency.setValueAtTime(freq, a.currentTime);
-      osc.connect(gain);
-      osc.start();
+    let key = e.key.toLowerCase();
+
+    if (pressedKeys[key]) return;
+    if (validKey(key)) {
+      startSound(key);
+      
       drawKeyboard();
     }
   });
   window.addEventListener("keyup", function(e) {
-    if (osc) {
-      osc.disconnect();
-      osc.stop();
-    }
-    pressedKey = null;
+    let key = e.key.toLowerCase();
+    stopSound(key);
+
     drawKeyboard();
+  });
+  document.getElementById("scale-selector").addEventListener("input", (e) => {
+    stopAllSounds();
+    setScale();
+    drawKeyboard();
+  });
+  window.addEventListener("blur", function(e) {
+    stopAllSounds();
+
   });
 }
 
+function startSound(key) {
+  pressedKeys[key] = true;
+
+  let freq = getFreq(key);
+  var sineWave = new Pizzicato.Sound({ 
+    source: 'wave', 
+    options: {
+        frequency: freq
+    }
+  });
+
+  sineWave.play();
+  keySounds[key] = sineWave;
+}
+function stopSound(key) {
+  let sound = keySounds[key];
+  if (sound) sound.stop();
+  keySounds[key] = null;
+  pressedKeys[key] = null;
+}
+function stopAllSounds() {
+  for (let key in keySounds) {
+    stopSound(key)
+  }
+}
+
+function setScale() {
+  let scale_selector = document.getElementById("scale-selector");
+  scale = scales[scale_selector.value];
+}
+function setSize() {
+  c.width = window.innerWidth*4/5;
+  c.height = c.width * canvasratio;
+}
 function getScaleKeys() {
   var arr = [];
   for (var i = 0; i < keys.length; i++) {
@@ -95,53 +145,79 @@ function getScaleKeys() {
 
 function validKey(key) {
   for (var i = 0; i < keys.length; i++) {
-    for (var j = 0; j < keys[i].length; j++) {
-      if (key === keys[i][j]) return true;
-    }
+      if (key === keys[i]) return true;
   }
   return false;
 }
 
-function getFreq(key) {
-  var semitones = 0;
-  for (var i = 0; i < keys.length; i++) {
-    for (var j = 0; j < keys[i].length; j++) {
-      if (key !== keys[i][j]) semitones += 1;
-      else return root * Math.pow(factor, semitones);
-    }
+function distFromRootKey(key) {
+  let rootKeyIndex = -1
+  let keyIndex = -1
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] === rootKey) rootKeyIndex = i;
+    if (keys[i] === key) keyIndex = i;
   }
+  return keyIndex - rootKeyIndex;
 }
 
+function getFreq(key) {
+  let dist = distFromRootKey(key);
+  let semitones = 0;
+
+  const stepDirection = dist < 0 ? -1 : 1;
+  for (let i = 0; i < stepDirection * dist; i++) {
+    semitones += stepDirection * scale[mod(stepDirection * i, scale.length)]; // use correct modulus
+  }
+  return rootFreq * Math.pow(factor, semitones);
+}
+
+function getNoteName(key) {
+  let dist = distFromRootKey(key);
+
+  const stepDirection = dist < 0 ? -1 : 1;
+  let noteSteps = 0;
+  for (let i = 0; i < stepDirection * dist; i++) {
+      noteSteps += stepDirection * scale[mod(stepDirection * i, scale.length)]; // use correct modulus
+  }
+  let rootNamePosition = noteNames.indexOf(rootName);
+  return noteNames[mod(rootNamePosition + noteSteps, noteNames.length)] // use correct modulus
+}
+
+const backgroundColor = "rgb("+Math.floor(Math.random()*256)+", " + Math.floor(Math.random()*256)+ "," + Math.floor(Math.random()*256) +")";
 function drawKeyboard() {
-  ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+", " + Math.floor(Math.random()*256)+ "," +Math.floor(Math.random()*256) +")";
+  ctx.fillStyle = backgroundColor;
   ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
 
+  const keySpacing = ctx.canvas.width / 13;
+  const keyPadding = 0.2 * keySpacing;
+  const keyDim = keySpacing - keyPadding;
 
-
-  var keySpacing = ctx.canvas.width / 13;
-  var keyPadding = 0.2 * keySpacing;
-  var keyDim = keySpacing - keyPadding;
-
+  let y = keyPadding;
+  let horiCount = 0;
+  let vertCount = 0;
   for (var i = 0; i < keys.length; i++) {
-    var y = keySpacing * i + keyPadding;
-    for (var j = 0; j < keys[i].length; j++) {
-      var x = keySpacing * j + keyPadding + i * keySpacing/3;
+      var x = keySpacing * horiCount + keyPadding + vertCount * keySpacing/3;
       ctx.strokeStyle = "rgb(255,255,255)";
       ctx.lineWidth = 2;
-      if (keys[i][j] === pressedKey) {
+      if (pressedKeys[keys[i]]) {
         ctx.fillStyle = "rgb(0,255,255)";
-      } else if (scaleKeys.includes(keys[i][j])) {
-        ctx.fillStyle = "rgb(255,0,0)";
       } else {
         ctx.fillStyle = "rgb(0,0,0)";
       }
-      ctx.fillRect(x,y, keyDim, keyDim);
-      //ctx.stroke();
-      //ctx.fill();
+      ctx.fillRect(x, y, keyDim, keyDim);
       ctx.fillStyle = "rgb(255,255,255)";
       ctx.textBaseline = "top";
       ctx.font = keyDim/4 + "px arial";
-      ctx.fillText(keys[i][j].toUpperCase(), x + keyDim/4, y + keyDim/4);
-    }
+      ctx.fillText(getNoteName(keys[i]), x + keyDim/4, y + keyDim/4);
+      horiCount++;
+      if (keys[i] === 'p' || keys[i] === ';' || keys[i] === '.') {
+        y += keySpacing;
+        horiCount = 0;
+        vertCount++;
+      }
   }
+}
+
+function mod(a, b) {
+  return ((a % b) + b) % b;
 }
